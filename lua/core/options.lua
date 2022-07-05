4a74707a47c27be844ab26aa61b32559cfd51969
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-05 17:43:42 +0100

    Added options.lua to lua/core

---
+++ lua/core/options.lua
@@ -0,0 +1,9 @@
+vim.opt.expandtab     = true
+vim.opt.shiftwidth    = 4
+vim.opt.tabstop       = 4
+vim.opt.hidden        = true
+vim.opt.termguicolors = true
+vim.opt.wrap          = false
+vim.opt.linespace     = 8
+vim.opt.cursorline    = true
+vim.opt.wildignore    = '.git, *.obj, */node_modules'
