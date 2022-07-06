f54a46127d697f7ba018755b8e352b58d67a287c
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-06 17:23:34 +0100

    Added Telescope mappings

--- lua/core/key-bindings.lua
+++ lua/core/key-bindings.lua
@@ -3,4 +3,5 @@ vim.keymap.set('n', '<Leader>nn', ':NvimTreeToggle<CR>')
 
 vim.keymap.set('n', '<Leader>as', ':colorscheme ')
 
---vim.api.nvim_set_keymap('i', '<Tab>', 'v:lua.smart_tab()', {expr = true, noremap = true})
+vim.keymap.set('n', '<Leader>ff', ':Telescope find_files<CR>')
+vim.keymap.set('n', '<Leader>lg', ':Telescope live_grep<CR>')
