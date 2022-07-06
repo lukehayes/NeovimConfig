226cc45312dcc64cf702d65cb245901a8bb28749
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-06 17:23:51 +0100

    Added mapping for Git commit.

--- lua/core/key-bindings.lua
+++ lua/core/key-bindings.lua
@@ -5,3 +5,6 @@ vim.keymap.set('n', '<Leader>as', ':colorscheme ')
 
 vim.keymap.set('n', '<Leader>ff', ':Telescope find_files<CR>')
 vim.keymap.set('n', '<Leader>lg', ':Telescope live_grep<CR>')
+
+vim.keymap.set('n', '<Leader>gg', ':Git commit -m ""<left>')
+
