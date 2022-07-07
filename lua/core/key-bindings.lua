d2536fc826a9fc78c73572c46da33add8c681e79
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-07 09:14:11 +0100

    Added movment mappings

--- lua/core/key-bindings.lua
+++ lua/core/key-bindings.lua
@@ -12,3 +12,8 @@ vim.keymap.set('', '<Leader>o', ':BufExplorer<CR>')
 
 vim.keymap.set('', '<Leader>bd', ':BClose<CR>')
 
+-- Movement
+vim.keymap.set('n', '<C-h>', '<C-w>h>', {noremap = true, silent = true})
+vim.keymap.set('n', '<C-j>', '<C-w>j>', {noremap = true, silent = true})
+vim.keymap.set('n', '<C-k>', '<C-w>k>', {noremap = true, silent = true})
+vim.keymap.set('n', '<C-l>', '<C-w>l>', {noremap = true, silent = true})
