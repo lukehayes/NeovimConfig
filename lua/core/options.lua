6c914ef2ecb7ca41a181fe15c4baa9226b655bf8
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-07 18:30:27 +0100

    Added listchars settings

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,9 +1,12 @@
-vim.opt.expandtab     = true
-vim.opt.shiftwidth    = 4
-vim.opt.tabstop       = 4
-vim.opt.hidden        = true
-vim.opt.termguicolors = true
-vim.opt.wrap          = false
-vim.opt.linespace     = 8
-vim.opt.cursorline    = true
-vim.opt.wildignore    = '.git, *.obj, */node_modules'
+vim.opt.expandtab       = true
+vim.opt.shiftwidth      = 4
+vim.opt.tabstop         = 4
+vim.opt.expandtab       = false
+vim.opt.hidden          = true
+vim.opt.termguicolors   = true
+vim.opt.wrap            = false
+vim.opt.linespace       = 8
+vim.opt.cursorline      = true
+vim.opt.wildignore      = '.git, *.obj, */node_modules'
+vim.opt.listchars       = { space = '_', tab = '>~' }
+vim.opt.list            = true
