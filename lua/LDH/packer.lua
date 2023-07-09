cf8f656a5c7ef736f0a402d84ef9df504e38566d
Luke Hayes <lukepurplehayes@gmail.com>
2023-07-09 19:19:11 +0100

    Added mason.nvim to project.

--- lua/LDH/packer.lua
+++ lua/LDH/packer.lua
@@ -13,4 +13,9 @@ return require('packer').startup(function(use)
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 
+	use {
+		"williamboman/mason.nvim",
+		run = ":MasonUpdate" -- :MasonUpdate updates registry contents
+	}
+
 end)
