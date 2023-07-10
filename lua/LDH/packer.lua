6ab368bdfa6760dabfc55b44f4f07edd58abed4f
lukehayes <lukepurplehayes@gmail.com>
2023-07-10 18:12:53 +0100

    Added LSP integration

--- lua/LDH/packer.lua
+++ lua/LDH/packer.lua
@@ -21,11 +21,34 @@ return require('packer').startup(function(use)
 	-- 
 	use {'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}}
 
+	-- LSP
+	-- 
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{                                      -- Optional
+			'williamboman/mason.nvim',
+			run = function()
+				pcall(vim.cmd, 'MasonUpdate')
+			end,
+		},
+		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+		-- Autocompletion
+		{'hrsh7th/nvim-cmp'},     -- Required
+		{'hrsh7th/cmp-nvim-lsp'}, -- Required
+		{'L3MON4D3/LuaSnip'},     -- Required
+	}
+}
+
+
 	-- Colour Schemes
 	-- 
-       use { 'savq/melange-nvim' }
-       use { 'rose-pine/neovim'  }
-
+	use { 'savq/melange-nvim' }
+	use { 'rose-pine/neovim'  }
 
 end)
 
