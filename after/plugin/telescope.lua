21695dce063d8c9942612166a915315610d9191a
lukehayes <lukepurplehayes@gmail.com>
2023-07-10 17:07:15 +0100

    Added shiftwifth and tabstop settings.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,17 @@
 local builtin = require('telescope.builtin')
 
+require('telescope').setup {
+  pickers = {
+    colorscheme = {
+      enable_preview = true
+    }
+  },
+  pickers.colorscheme.enable_preview = true
+}
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>as', builtin.colorscheme, {})
+
--- lua/LDH/mappings.lua
+++ lua/LDH/mappings.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+vim.g.shiftwidth = 4
+vim.g.tabstop    = 4
