8c0bc533715a85d0f93acc5918cf21dc262c119b
lukehayes <lukepurplehayes@gmail.com>
2023-07-10 18:30:00 +0100

    Added the key remaps that I like

--- lua/LDH/mappings.lua
+++ lua/LDH/mappings.lua
@@ -1,5 +1,26 @@
+-- Leader key
+--
 vim.g.mapleader = " "
+
+-- Nice defaults, indentation etc.
+--
+vim.g.shiftwidth  = 4
+vim.g.tabstop     = 4
+vim.g.softtabstop = 4
+vim.g.expandtab   = true
+vim.opt.wrap      = false
+
+vim.opt.nu             = true
+vim.opt.relativenumber = true
+vim.opt.incsearch      = true
+vim.opt.termguicolors  = true
+
+-- Key remaps
+--
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
-vim.g.shiftwidth = 4
-vim.g.tabstop    = 4
+local map = vim.api.nvim_set_keymap
+map('n', '<C-l>', '<C-w>l', {noremap = true})
+map('n', '<C-h>', '<C-w>h', {noremap = true})
+map('n', '<C-j>', '<C-w>j', {noremap = true})
+map('n', '<C-k>', '<C-w>k', {noremap = true})
