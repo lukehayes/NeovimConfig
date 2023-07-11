e4ba9e51f0e8d84355d1084797de743a2fbb7904
lukehayes <lukepurplehayes@gmail.com>
2023-07-11 16:58:23 +0100

    Changed Neotree folder icons to a plus and cross.

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -37,8 +37,8 @@ require("neo-tree").setup({
 				expander_highlight = "NeoTreeExpander",
 			},
 			icon = {
-				folder_closed = "",
-				folder_open = "",
+				folder_closed = "",
+				folder_open = "",
 				folder_empty = "ﰊ",
 				-- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
 				-- then these will never be used.
