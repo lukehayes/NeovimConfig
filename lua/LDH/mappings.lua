b0f6f60891c54cb7dec09b9388195bdc2b84dd5f
lukehayes <lukepurplehayes@gmail.com>
2023-07-18 16:40:12 +0100

    Added mappings for removing highlight after search

--- lua/LDH/mappings.lua
+++ lua/LDH/mappings.lua
@@ -20,12 +20,20 @@ vim.go.termguicolors  = true
 vim.keymap.set("n", "<leader>z", ':ZenMode<CR>')
 
 local map = vim.api.nvim_set_keymap
-map('n', '<C-l>', '<C-w>l', {noremap = true})
-map('n', '<C-h>', '<C-w>h', {noremap = true})
-map('n', '<C-j>', '<C-w>j', {noremap = true})
-map('n', '<C-k>', '<C-w>k', {noremap = true})
-map('n', '<leader>c', '<C-w>c', {noremap = true})
-map('n', '<leader>bd', ':bdelete<CR>', {noremap = true})
+local opt = {noremap = true}
+
+-- Window Movement
+map('n', '<C-l>', '<C-w>l', opt)
+map('n', '<C-h>', '<C-w>h', opt)
+map('n', '<C-j>', '<C-w>j', opt)
+map('n', '<C-k>', '<C-w>k', opt)
+
+-- Window Closing
+map('n', '<leader>c', '<C-w>c', opt)
+map('n', '<leader>bd', ':bdelete<CR>', opt)
+
+-- Disbale Highlight
+map('n', '<leader>h', ':noh<CR>', opt)
 
 local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
 vim.keymap.set("i", "<TAB>", 'pumvisible() ? "<C-n>" : "<TAB>"', opts)
