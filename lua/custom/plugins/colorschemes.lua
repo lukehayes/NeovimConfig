fae7f394bc9902df0fbfe6c3994377db1a1a5fa7
Luke Hayes <lukepurplehayes@gmail.com>
2023-07-24 10:41:32 +0100

    Added many colorschemes to project.

---
+++ lua/custom/plugins/colorschemes.lua
@@ -0,0 +1,30 @@
+return  {
+  'folke/tokyonight.nvim',
+  'whatyouhide/vim-gotham',
+
+  'savq/melange-nvim',
+  'rose-pine/neovim',
+  'wadackel/vim-dogrun',
+  'NLKNguyen/papercolor-theme',
+  'cocopon/iceberg.vim',
+  'jaredgorski/fogbell.vim',
+  'atelierbram/Base2Tone-nvim',
+  'rakr/vim-two-firewatch',
+  'liuchengxu/space-vim-dark',
+  'TheNiteCoder/mountaineer.vim',
+  'Shatur/neovim-ayu',
+  'Badacadabra/vim-archery',
+  'gilgigilgil/anderson.vim',
+  'oxfist/night-owl.nvim',
+  'sainnhe/gruvbox-material',
+  'sainnhe/sonokai',
+  'sainnhe/everforest',
+  'sainnhe/edge',
+
+  {
+          "kyazdani42/blue-moon",
+          config = function()
+                  vim.opt.termguicolors = true
+          end
+  }
+}
