156dab6ade536b883a2e41c3ae4fea53b0796a68
lukehayes <lukepurplehayes@gmail.com>
2025-05-14 18:13:32 +0100

    feat: add smear_cursor plugin

--- init.lua
+++ init.lua
@@ -237,6 +237,31 @@ require('lazy').setup({
     "Tetralux/odin.vim"
   },
 
+  {
+    "sphamba/smear-cursor.nvim",
+
+    opts = {
+      -- Smear cursor when switching buffers or windows.
+      smear_between_buffers = true,
+
+      -- Smear cursor when moving within line or to neighbor lines.
+      -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
+      smear_between_neighbor_lines = true,
+
+      -- Draw the smear in buffer space instead of screen space when scrolling
+      scroll_buffer_space = true,
+
+      -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+      -- Smears will blend better on all backgrounds.
+      legacy_computing_symbols_support = false,
+
+      -- Smear cursor in insert mode.
+      -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
+      smear_insert_mode = true,
+
+    },
+  },
+
   --
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
---
+++ lua/custom/plugins/smear-cursor.lua
@@ -0,0 +1,6 @@
+require("smear_cursor").setup({
+    stiffness               = 0.8,
+    trailling_stiffness     = 0.5,
+    distance_stop_animating = 0.5,
+    never_draw_over_target  = false
+})
