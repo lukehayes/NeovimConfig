9123afc350ff95840ebaeebb6bbde76edb28d001
lukehayes <lukepurplehayes@gmail.com>
2025-05-15 09:06:32 +0100

    fix: corrected smear-cursor.lua

--- lua/custom/plugins/smear-cursor.lua
+++ lua/custom/plugins/smear-cursor.lua
@@ -1,6 +1,12 @@
-require("smear_cursor").setup({
-    stiffness               = 0.8,
-    trailling_stiffness     = 0.5,
-    distance_stop_animating = 0.5,
-    never_draw_over_target  = false
-})
+return {
+    "sphamba/smear-cursor.nvim",
+
+    config = function()
+        require("smear_cursor").setup({
+            stiffness               = 0.8,
+            trailling_stiffness     = 0.5,
+            distance_stop_animating = 0.5,
+            never_draw_over_target  = false
+        })
+    end
+}
