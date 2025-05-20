c7fcfb2ce83e837375620b890b4990c03ed35c9e
lukehayes <lukepurplehayes@gmail.com>
2025-05-20 14:49:03 +0100

    fix: autocmds and mappings now working correctly

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,27 +1,34 @@
 local map = vim.api.nvim_set_keymap
-local opt = {noremap = true}
+local opt = { noremap = true }
 
 -- Telescope Color Scheme Picker
 -- vim.keymap.set('n', '<leader>as', require('telescope.builtin').colorscheme, { desc = '[?] Find recently opened files' })
 
 -- Window Movement
-map('n', '<leader>wl', '<C-w>l', opt)
-map('n', '<leader>wh', '<C-w>h', opt)
-map('n', '<leader>wj', '<C-w>j', opt)
-map('n', '<leader>wk', '<C-w>k', opt)
+--map('n', '<leader>wl', '<C-w>l', opt)
+--map('n', '<leader>wh', '<C-w>h', opt)
+--map('n', '<leader>wj', '<C-w>j', opt)
+--map('n', '<leader>wk', '<C-w>k', opt)
 
-map('n', '<leader>wv', '<C-w>v', opt)
-map('n', '<leader>ws', '<C-w>s', opt)
+map("n", "<leader>wh", "<C-w><C-h>", { desc = "Move focus to the left window" })
+map("n", "<leader>wl", "<C-w><C-l>", { desc = "Move focus to the right window" })
+map("n", "<leader>wj", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+map("n", "<leader>wk", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+map("n", "<leader>wv", "<C-w>v", { desc = "Create new vertical pane" })
+map("n", "<leader>ws", "<C-w>s", { desc = "Create new horiztonal pane" })
 
 -- Window Closing
-map('n', '<leader>c', '<C-w>c', opt)
-map('n', '<leader>bd', ':bdelete<CR>', opt)
-map('n', '<leader>ba', ':%bd<CR>', opt) -- Close all buffers
+map("n", "<leader>c", "<C-w>c", opt)
+map("n", "<leader>bd", ":bdelete<CR>", opt)
+map("n", "<leader>ba", ":%bd<CR>", opt) -- Close all buffers
 
 -- Buffer Navigation
-map('n', '<S-h>', ':bn<CR>', opt)
-map('n', '<S-l>', ':bp<CR>', opt)
+map("n", "<S-h>", ":bn<CR>", opt)
+map("n", "<S-l>", ":bp<CR>", opt)
 
 -- Telescope
-map('n', '<leader>t', ':Telescope<CR>', opt)
+map("n", "<leader>t", ":Telescope<CR>", { desc = "Open Telescope" })
 
+print("Mappings Loaded")
+
+return {}
