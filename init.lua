58d283dfe8d49fea619911798fde3c7f3d07c75a
lukehayes <lukepurplehayes@gmail.com>
2025-05-20 15:29:23 +0100

    feat: add Telescope colorscheme mapping

--- init.lua
+++ init.lua
@@ -422,6 +422,8 @@ require("lazy").setup({
 
       -- See `:help telescope.builtin`
       local builtin = require("telescope.builtin")
+
+      vim.keymap.set("n", "<leader>as", builtin.colorscheme, { desc = "[A] Select a colorscheme" })
       vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
       vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
       vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
