990c0c03d9755d563f85d7bf0c33f31d5d6143f6
lukehayes <lukepurplehayes@gmail.com>
2025-05-20 17:26:20 +0100

    feat: add colors.lua

---
+++ lua/custom/plugins/colors.lua
@@ -0,0 +1,13 @@
+return  {
+
+    'folke/tokyonight.nvim',
+    'rakr/vim-two-firewatch',
+    "aperezdc/vim-elrond",
+    "miikanissi/modus-themes.nvim",
+    "cvigilv/patana.nvim",
+    "wolandark/NotePad-Vim",
+    "alexxGmZ/e-ink.nvim",
+
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+}
