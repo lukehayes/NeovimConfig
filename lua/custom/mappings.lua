058f5e2126307487a9069b0c32c2c81536eceb33
lukehayes <lukepurplehayes@gmail.com>
2025-05-23 13:01:24 +0100

    fix: add <leader>cc comment mapping

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -18,13 +18,16 @@ map("n", "<leader>wv", "<C-w>v", { desc = "Create new vertical pane" })
 map("n", "<leader>ws", "<C-w>s", { desc = "Create new horiztonal pane" })
 
 -- Window Closing
-map("n", "<leader>c", "<C-w>c", opt)
+-- map("n", "<leader>c", "<C-w>c", opt)
 map("n", "<leader>bd", ":bdelete<CR>", opt)
 map("n", "<leader>ba", ":%bd<CR>", opt) -- Close all buffers
 
 -- Buffer Navigation
 map("n", "<S-h>", ":bn<CR>", opt)
 map("n", "<S-l>", ":bp<CR>", opt)
+
+-- Comments
+map("n", "<leader>cc", "gcc", { desc = "Comment line", noremap = false })
 
 -- Telescope
 map("n", "<leader>t", ":Telescope<CR>", { desc = "Open Telescope" })
